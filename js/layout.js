b8f58f33fae341fee4380743ed6b5d870f22e8fd
mattskeoch <mattskeoch@gmail.com>
2021-01-09 18:19:22 +0800

    Removed console.log

--- js/layout.js
+++ js/layout.js
@@ -2,7 +2,6 @@ const wrapper = document.querySelector('.wrapper');
 const leftBtn = document.querySelector('.left');
 const centerBtn = document.querySelector('.center');
 const fullBtn = document.querySelector('.full');
-console.log(wrapper)
 
 let leftAlign = () => {
   if (wrapper.classList.contains('wrapper') || wrapper.classList.contains('wrapper-full')) {
